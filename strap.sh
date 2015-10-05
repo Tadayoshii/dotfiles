02a379ce51e5777313780fd878510c7d4203fac6
Tadayoshii <D.Voigt1993@gmail.com>
2015-10-05 14:37:25 +0200

    Update strap.sh
    Update vundle

--- strap.sh
+++ strap.sh
@@ -5,12 +5,13 @@ echo -e "\e[31mStrapping \e[36mall \e[94mda \e[32mdots \e[0m"
 echo -e "Setting up vim"
 
 #TODO: Add as submodule not plain git it here
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/vundle
 cp vimrc ~/.vimrc
-vim +BundleInstall +qall
-sh ~/.vim/bundle/YouCompleteMe/install.sh
-cp libclang.so ~/.vim/bundle/YouCompleteMe/third_party/ycmd/libclang.so
-cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
+vim +PluginInstall +qall
+#sh ~/.vim/bundle/YouCompleteMe/install.sh
+#cp libclang.so ~/.vim/bundle/YouCompleteMe/third_party/ycmd/libclang.so
+#cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
 #mplayer conf
 
 #Xressources
